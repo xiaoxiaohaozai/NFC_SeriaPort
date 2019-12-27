58a3185a557d6eae6100d0bb13008d911f5a5d06
陈豪 <chenh@topband.com.cn>
2019-12-27 14:18:36 +0800

    创建项目
    Change-Id: I967230a4599b0cf4d3d85a3f06b8c44d5902909c

---
+++ app/src/main/java/com/hc/tools/nfc_serialport/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hc.tools.nfc_serialport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
